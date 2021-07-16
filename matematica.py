e47d5fdae43b32a52ffc03d55558765ca3e8237c
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-16 19:54:13 -0300

    prioridades (matematica)
    a importancia da prioridade

---
+++ matematica.py
@@ -0,0 +1,19 @@
+#exerciocio numero 1 connvertar 20+10*30 resultado tem que da 900
+print ((20+10) *30) 
+# o  resultado desse  jeito deu 900
+# mas  eu estava tentado  fazer 
+#print (20+10*30) = 320, errado .. 
+
+# converta 4**2 / 30
+print ((4**2)/30)
+
+#converta (9**4+2*6-1)
+print ((9**4 +2)* 6-1)
+# deu  certo do  jeito assim print ((9**4 +2)*6-1)
+#desse  jeito aqui de  baixo da outro valor print ((9**4 +2)*(6-1))
+print ((9**4 +2)*(6-1))
+
+
+#exercicio 2 digite a seguinte expressao no interpretador 
+# 40%3*10**2+1-10*4/2
+print(40%3*10**2+1-10*4/2)
