aeb203c6243a181e530960af5d92f0495cc6693b
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-17 18:11:58 -0300

    Logicas com varias
    usando logicas  com variasves

---
+++ variaves_usando_logica.py
@@ -0,0 +1,13 @@
+materia1 = 7
+materia2 = 7
+materia3 = 7
+
+if materia1 >= 7 :
+    print('Show de bola 1')
+
+if materia2 >= 7 :
+    print('Show de bola 2') 
+
+if materia3 >= 7 :
+    print('Show de bola 3')
+
