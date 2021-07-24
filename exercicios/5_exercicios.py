4d8190e6dd1ef94388692e15a6050410c7cceb7f
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-24 09:35:40 -0300

    Create 5_exercicios.py

---
+++ exercicios/5_exercicios.py
@@ -0,0 +1,12 @@
+n1 = int(input("digite 1  numero : "))
+n2 = int(input*"digite o segfundo numero :")
+n3 = int (input("digite o terceiro :")
+
+if (n1 > n2 and n3)
+print(f"{n1} é maior que {n2} e {n3}")
+
+if(n2 > n1 and n3)
+    print(f"{n2} é  maior que {n1} e  {n3}")
+if(n3 > n2 and n1)
+    print (f"{n3} é mair que {n2} e  {n1}")
+
