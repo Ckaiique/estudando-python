f917b9fc09d011ccd37ba777260ee2d2f831beba
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-25 08:41:05 -0300

    Update 5_exercicios.py
    consegui resolver a questao, onde  tenho que fazer  aparecer o maior  e  o menor  numero digitado

--- exercicios/5_exercicios.py
+++ exercicios/5_exercicios.py
@@ -1,12 +1,20 @@
-n1 = int(input("digite 1  numero : "))
-n2 = int(input*"digite o segfundo numero :")
-n3 = int (input("digite o terceiro :")
+n1 = int(input("n1  numero : "))
+n2 = int(input("n2 o segundo numero :"))
+n3 = int(input("n3 o terceiro :"))
 
-if (n1 > n2 and n3)
-print(f"{n1} é maior que {n2} e {n3}")
+if (n1 > n2 > n3):
+    print(f"n1  maior {n1} ")
 
-if(n2 > n1 and n3)
-    print(f"{n2} é  maior que {n1} e  {n3}")
-if(n3 > n2 and n1)
-    print (f"{n3} é mair que {n2} e  {n1}")
+if(n2 > n1 > n3):
+    print(f"n2  maior {n2} ")
+if(n3 > n2 > n1):
+    print (f"n3  maior  {n3}")
 
+if (n1< n2 < n3):
+    print(f"o valor do n1 menor {n1}")
+
+if(n2 < n1 < n3):
+    print(f"o valor do n2 menor {n2}")
+
+if ( n3 < n2 < n1):
+    print( f"o valor do n3 menor {n3}")
