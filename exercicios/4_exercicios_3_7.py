dc97e72b6e9ab08558a5f91f9fd12f78e1a8c3de
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-25 09:04:35 -0300

    Update 4_exercicios_3_7.py
    Aumento de salario

--- exercicios/4_exercicios_3_7.py
+++ exercicios/4_exercicios_3_7.py
@@ -1,2 +1,14 @@
-print ('faça um programa que calcule o aumento de um salario. Ele deve solicitar o valor do salario e a porcentagem do aumento. exiba o valor do  aumento e do novo salario.')
+ #aumento de salario 
+
+n1 = int(input("digite  o seu salario:"))
+salario_base=1250
+resultado = 0
+
+if(n1>salario_base):
+    resultado = resultado + ((10 * n1) / 100)
+    print(f'você teve um auemnto de  R${resultado}')
+
+if(n1 <= salario_base):
+    resultado = resultado + ((15 * n1) / 100)
+    print(f'você teve um auemnto de  R${resultado}')
 
