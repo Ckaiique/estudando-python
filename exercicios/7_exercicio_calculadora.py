9802437d53d4d7ea695366853e824d003fd2ed6f
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-25 13:57:29 -0300

    Create 7_exercicio_calculadora.py
    criando uma calculadora usando elif

---
+++ exercicios/7_exercicio_calculadora.py
@@ -0,0 +1,17 @@
+n1 = float(input("Digite o primeiro numero:"))
+op = input("Digite o operador que você deseja usar:")
+n2 = float(input("Digite o segundo numero:"))
+
+if op == "+":
+    result = n1 + n2
+elif op =="-":
+    result =  n1 - n2
+elif op == "*":
+    result = n1 * n2 
+elif op == "/":
+    result == n1 / n2
+else:
+    print("Digite um operador  Valido igual á esses (+), (-), (*), (/)")
+print (f"O resultado da Sua Operaçao é {result}")
+    
+    
