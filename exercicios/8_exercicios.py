f7e65cee25b6bbe0041967bc5bb7b85b8d8d649e
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-26 14:10:21 -0300

    Update 8_exercicios.py
    Consegui concluir o exercicio

--- exercicios/8_exercicios.py
+++ exercicios/8_exercicios.py
@@ -4,10 +4,11 @@ salario=  float(input('Digite  o seu salario:'))
 anos_pagar= float(input('Digite a quantidade de anos que você deseja pagar :'))
 
 meses_pagar= anos_pagar * 12
+valor_mensal = ((30 * valor_casa)/ 100)
 calculo_prestaçao =  ((30 * salario)/100)
 
-if (calculo_prestaçao <= salario ):
-    resultado = calculo_prestaçao + valor_casa / meses_pagar
+if (calculo_prestaçao <= valor_mensal ):
+    resultado = valor_mensal / meses_pagar
     print('O emprestimo foi aprovado !')
 else:
     print('Infelismente não pode ser  aprovado !')
