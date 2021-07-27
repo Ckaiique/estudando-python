489311bef63097bc2cc0a99de329be6252731396
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2021-07-27 07:23:24 -0300

    questao usando while

--- exercicios/12_exercicios_while.py
+++ exercicios/12_exercicios_while.py
@@ -1,7 +1,7 @@
-
 x = int(input('Digite o incio da tabua :'))
 n2 = int(input('Digite o valor final que voce queria: '))
 while x <= n2:
+    
     cl= n2 * x
     print(f"{n2} * {x} = {cl}")
     x = x + 1
---
+++ exercicios/13_exercicios_while.py
@@ -0,0 +1,6 @@
+
+x = int(input('Digite o primeiro numero :'))
+n2 = int(input('Digite o segundo numero :'))
+while x <= n2:
+    cl= x * n2 
+    x = x + 1
